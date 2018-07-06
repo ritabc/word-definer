316ad634ac76175a066de7d165649a8ac960ef8f
ritabc <ritabc@gmail.com>
2018-07-06 15:14:55 -0700

    Pass return button test

--- spec/word-definer_integration_spec.rb
+++ spec/word-definer_integration_spec.rb
@@ -4,12 +4,23 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 
-describe('from the /word/:word page', {:type => :feature}) do
-  it('user can click Return to Word List button') do
+describe('from the / page', {:type => :feature}) do
+  it('user can Add a word') do
     visit('/')
     fill_in 'add_word', with: 'tree'
     click_button('Add Word')
     expect(page).to have_content('tree')
+    Word.clear
+  end
+end
+describe('from the /word/:word page', {:type => :feature}) do
+  it('user can return to the list from the definitions page') do
+    visit('/')
+    fill_in 'add_word', with: 'tree'
+    click_button('Add Word')
+    click_link('tree')
+    click_button("Return to Word List")
+    expect(page).to have_content('Add')
     Word.clear
   end
 end
