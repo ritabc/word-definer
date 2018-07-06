fd06f0995960e78a1fa6e429ee24f15fdb5a6aca
ritabc <ritabc@gmail.com>
2018-07-06 14:12:31 -0700

    add non-yet-functioning return button

--- spec/word-definer_integration_spec.rb
+++ spec/word-definer_integration_spec.rb
@@ -2,3 +2,12 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+
+describe('from the /word/:word page', {:type => :feature}) do
+  it('user can click Return to Word List button') do
+    visit('/word/:word')
+    click_button('Return to Word List')
+    expect(page).to have_content('Room 1')
+  end
+end
